2de861607dcfa0b2fb6c8150d145cf573e0e1684
AngelChaos26 <angelchaos_26@hotmail.com>
2024-08-23 19:09:56 +0000

    Adding configuration, adding tailwind and stimulus configuration

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,6 +1,7 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "application"
+pin "application", to: "application.js"
+pin "tailwindcss", to: "https://cdn.tailwindcss.com", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
